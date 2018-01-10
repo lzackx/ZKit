230c662eb19432484705687a542ec1306a9b5038
lzackx <zackx@foxmail.com>
2018-01-10 16:18:00 +0800

    Update zprint

--- ZKit/ZKit/NSObject+ZKit.swift
+++ ZKit/ZKit/NSObject+ZKit.swift
@@ -10,13 +10,18 @@ import Foundation
 
 extension NSObject {
     
-    public func zprint(_ items: Any..., separator: String = " ", terminator: String = "") {
+    public func zprint(_ items: Any..., separator: String = " ", terminator: String = "\n") {
         #if DEBUG
             let date = Date()
             let timeFormatter = DateFormatter()
             timeFormatter.dateFormat = "yyy-MM-dd HH:mm:ss.SSS"
             let now = timeFormatter.string(from: date) as String
-            print("\(NSStringFromClass(self.classForCoder))", "\(now):", items, separator, terminator)
+            print("\(NSStringFromClass(self.classForCoder))", "\(now):", terminator: "")
+            for item in items {
+                print(separator, terminator: "")
+                print(item, terminator: "")
+            }
+            print(terminator: terminator)
         #endif
     }
     
