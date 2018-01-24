8a6af47e07ded643546d8981e3c90294f2120142
lzackx <zackx@foxmail.com>
2018-01-24 10:48:26 +0800

    Update Date+ZKit

--- ZKit/ZKit/Date+ZKit.swift
+++ ZKit/ZKit/Date+ZKit.swift
@@ -20,12 +20,31 @@ extension Date {
                                                    .minute,
                                                    .second]
         var dateComponents = calendar.dateComponents(components, from: self)
+        let zone = TimeZone.current
         dateComponents.hour = 0
         dateComponents.minute = 0
-        dateComponents.second = 0
+        dateComponents.second = zone.secondsFromGMT()
         
         let zeroDate = calendar.date(from: dateComponents)!
         
         return zeroDate
+    }
+    
+    public func zoneDate() -> Date {
+        
+        let calendar: Calendar = Calendar.current
+        let components: Set<Calendar.Component> = [.year,
+                                                   .month,
+                                                   .day,
+                                                   .hour,
+                                                   .minute,
+                                                   .second]
+        var dateComponents = calendar.dateComponents(components, from: self)
+        let zone = TimeZone.current
+        dateComponents.second = zone.secondsFromGMT()
+        
+        let zoneDate = calendar.date(from: dateComponents)!
+        
+        return zoneDate
     }
 }
