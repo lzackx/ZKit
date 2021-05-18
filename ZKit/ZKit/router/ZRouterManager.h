61609d8f3d77f10923e0e54c1338af539d8bf149
lzackx <lzackx@lzackx.com>
2021-05-18 11:48:31 +0800

    update: ZRouterManager

--- ZKit/ZKit/router/ZRouterManager.h
+++ ZKit/ZKit/router/ZRouterManager.h
@@ -6,10 +6,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Zouter/Zouter.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ZRouterManager : NSObject
+
++ (void)initializeWithScheme:(NSString *)scheme;
++ (void)registerDefaultRouters;
+
+// Registration
+// targetActionURL: scheme://[target]/[action]?[params]
++ (void)registerWithPattern:(NSString *)pattern
+			targetActionURL:(NSString *)targetActionURL
+			   synchronizly:(BOOL)synchronizly
+				   priority:(ZouterCommandPriority)priority
+				 willExcute:(ZouterCommandCallback __nullable)willExcute
+				  didExcute:(ZouterCommandCallback __nullable)didExcute
+				 parameters:(NSDictionary<NSString *, NSObject *> * __nullable)parameters;
+
++ (void)registerCommand:(ZouterCommand *)command;
+
+// Perform
++ (void)performURLString:(NSString * _Nullable)urlString;
++ (void)performURL:(NSURL * _Nullable)url;
 
 @end
 
--- ZKit/ZKit/router/ZRouterManager.m
+++ ZKit/ZKit/router/ZRouterManager.m
@@ -6,7 +6,51 @@
 //
 
 #import "ZRouterManager.h"
+#import <Zouter/Zouter.h>
 
 @implementation ZRouterManager
+
++ (void)initializeWithScheme:(NSString *)scheme {
+	[Zouter initializeWithScheme:scheme];
+}
+
++ (void)registerDefaultRouters {
+	[Zouter sharedRouter];
+}
+
+// Registration
+// targetActionURL: scheme://[target]/[action]?[params]
+
++ (void)registerWithPattern:(NSString *)pattern
+			targetActionURL:(NSString *)targetActionURL
+			   synchronizly:(BOOL)synchronizly
+				   priority:(ZouterCommandPriority)priority
+				 willExcute:(ZouterCommandCallback)willExcute
+				  didExcute:(ZouterCommandCallback)didExcute
+				 parameters:(NSDictionary<NSString *, NSObject *> *)parameters {
+	
+	ZouterCommand *command = [ZouterCommand new];
+	command.pattern = pattern;
+	command.taURL = targetActionURL;
+	command.synchronizly = synchronizly;
+	command.priority = priority;
+	command.willExcute = willExcute;
+	command.didExcute = didExcute;
+	command.parameters = parameters;
+	[ZRouterManager registerCommand:command];
+}
+
++ (void)registerCommand:(ZouterCommand *)command {
+	[[Zouter sharedRouter] registerCommand:command];
+}
+
+// Perform
++ (void)performURLString:(NSString * _Nullable)urlString {
+	[[Zouter sharedRouter] performURLString:urlString];
+}
+
++ (void)performURL:(NSURL * _Nullable)url {
+	[[Zouter sharedRouter] performURL:url];
+}
 
 @end
