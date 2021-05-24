76d7e7f43ad5d687f681da6bf4b613c1722c0b78
lzackx <lzackx@lzackx.com>
2021-05-24 16:08:57 +0800

    add: ZRequestManager

--- ZKit/Classes/request/ZRequestManager.h
+++ ZKit/Classes/request/ZRequestManager.h
@@ -11,6 +11,35 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ZRequestManager : NSObject
 
++ (void)launchRequestWithRequestMaxConcurrentOperationCount:(NSInteger)maxConcurrentOperationCount
+									 commonHeaderParameters:(nullable NSDictionary *)commonHeaderParameters
+									   commonBodyParameters:(nullable NSDictionary *)commonBodyParameters
+									 requestTimeoutInterval:(NSTimeInterval)requestTimeoutInterval
+							  responseAcceptableStatusCodes:(nullable NSIndexSet *)acceptableStatusCodes
+		   commonRequestTaskDidFinishCollectingMetricsBlock:(nullable void(^)(NSURLSession *session, NSURLSessionTask *task, NSURLSessionTaskMetrics * metrics))requestTaskDidFinishCollectingMetricsBlock
+						  commonRequestTaskDidCompleteBlock:(nullable void (^)(NSURLSession *session, NSURLSessionTask *task, NSError * _Nullable error))requestTaskDidCompleteBlock;
+
++ (void)launchNetworkReachabilityMonitorWithDomain:(nullable NSString *)domain
+							  statusChangeCallback:(nullable void (^)(NSInteger status))statusChangeCallback;
+
++ (NSString *)fetchCachedResponseFor:(NSString *)url;
+
++ (void)get:(NSString *)url
+	 header:(NSDictionary *)header
+ parameters:(NSDictionary *)parameters
+shouldCache:(BOOL)shouldCache
+  dataClass:(nullable Class)dataClass
+	success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+	failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
++ (void)post:(NSString *)url
+	  header:(NSDictionary *)header
+  parameters:(NSDictionary *)parameters
+ shouldCache:(BOOL)shouldCache
+   dataClass:(nullable Class)dataClass
+	 success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+	 failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- ZKit/Classes/request/ZRequestManager.m
+++ ZKit/Classes/request/ZRequestManager.m
@@ -6,7 +6,69 @@
 //
 
 #import "ZRequestManager.h"
+#import <Zequest/Zequest.h>
 
 @implementation ZRequestManager
+
++ (void)launchRequestWithRequestMaxConcurrentOperationCount:(NSInteger)maxConcurrentOperationCount
+									 commonHeaderParameters:(nullable NSDictionary *)commonHeaderParameters
+									   commonBodyParameters:(nullable NSDictionary *)commonBodyParameters
+									 requestTimeoutInterval:(NSTimeInterval)requestTimeoutInterval
+							  responseAcceptableStatusCodes:(nullable NSIndexSet *)acceptableStatusCodes
+		   commonRequestTaskDidFinishCollectingMetricsBlock:(nullable void(^)(NSURLSession *session, NSURLSessionTask *task, NSURLSessionTaskMetrics * metrics))requestTaskDidFinishCollectingMetricsBlock
+						  commonRequestTaskDidCompleteBlock:(nullable void (^)(NSURLSession *session, NSURLSessionTask *task, NSError * _Nullable error))requestTaskDidCompleteBlock {
+	[[Zequest shared] registerCommonRequestMaxConcurrentOperationCount:maxConcurrentOperationCount];
+	[[Zequest shared] registerCommonHeaderParameters:commonBodyParameters];
+	[[Zequest shared] registerCommonBodyParameters:commonBodyParameters];
+	[[Zequest shared] registerCommonRequestTimeoutInterval:requestTimeoutInterval];
+	[[Zequest shared] registerCommonResponseAcceptableStatusCodes:acceptableStatusCodes];
+	[[Zequest shared] registerCommonRequestTaskDidFinishCollectingMetricsBlock:requestTaskDidFinishCollectingMetricsBlock];
+	[[Zequest shared] registerCommonRequestTaskDidCompleteBlock:requestTaskDidCompleteBlock];
+	[[Zequest shared] launchCommonHTTPSessionManager];
+}
+
++ (void)launchNetworkReachabilityMonitorWithDomain:(nullable NSString *)domain
+							  statusChangeCallback:(nullable void (^)(NSInteger status))statusChangeCallback {
+	[[Zequest shared] launchReachabilityManagerWithDomain:domain statusChangeCallback:statusChangeCallback];
+}
+
++ (NSString *)fetchCachedResponseFor:(NSString *)url {
+	NSString *cr = [[Zequest shared] cachedResponseFor:url];
+	return cr;
+}
+
++ (void)get:(NSString *)url
+	 header:(NSDictionary *)header
+ parameters:(NSDictionary *)parameters
+shouldCache:(BOOL)shouldCache
+  dataClass:(nullable Class)dataClass
+	success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+	failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure {
+	[[Zequest shared] get:url
+				   header:header
+			   parameters:parameters
+			  shouldCache:shouldCache
+				dataClass:dataClass
+				 progress:nil
+				  success:success
+				  failure:failure];
+}
+
++ (void)post:(NSString *)url
+	  header:(NSDictionary *)header
+  parameters:(NSDictionary *)parameters
+ shouldCache:(BOOL)shouldCache
+   dataClass:(nullable Class)dataClass
+	 success:(nullable void (^)(NSURLSessionDataTask * _Nonnull, id _Nullable))success
+	 failure:(nullable void (^)(NSURLSessionDataTask * _Nullable, NSError * _Nonnull))failure {
+	[[Zequest shared] post:url
+					header:header
+				parameters:parameters
+			   shouldCache:shouldCache
+				 dataClass:dataClass
+				  progress:nil
+				   success:success
+				   failure:failure];
+}
 
 @end
